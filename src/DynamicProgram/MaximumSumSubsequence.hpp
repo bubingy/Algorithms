0104327233a136de69574bc4cfc0a6c014f48365
bubingy <769004837@qq.com>
2021-07-13 10:30:25 +0800

    add MaximumSumSubsequence

--- src/DynamicProgram/MaximumSumSubsequence.hpp
+++ src/DynamicProgram/MaximumSumSubsequence.hpp
@@ -1,16 +1,37 @@
 #include <assert.h>
+#include <vector>
+#include <list>
 
-template<typename t> std::vector<size_t>
+template<typename t> std::list<t>
 MaximumSumSubsequence(t* array, size_t array_size)
 {
 	assert(array_size > 0);
+	t max_sum = 0;
+	t temp_sum = 0;
+	size_t left_bound = 0;
+	size_t right_bound = 0;
+
+	std::vector<t> sum_records;
+
 	for (size_t i = 0; i < array_size; i++)
 	{
-		if (array[i] < 0)
-			continue;
-		for (size_t j = i; j < array_size; j++)
-		{
+		temp_sum = std::max(temp_sum + array[i], array[i]);
+		sum_records.push_back(temp_sum);
 
+		if (temp_sum > max_sum)
+		{
+			max_sum = temp_sum;
+			right_bound = i;
 		}
 	}
+
+	std::list<t> MaximumSumSubsequence;
+	for (size_t i = right_bound; i >= 0; i--)
+	{
+		if (sum_records[i] <= 0)
+			break;
+		MaximumSumSubsequence.push_front(array[i]);
+	}
+
+	return MaximumSumSubsequence;
 }
--- src/example.cxx
+++ src/example.cxx
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "MaximumSumSubsequence.hpp"
+#include "print.hpp"
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+	vector<int> array = { -2,1,-3,4,-1,2,1,-5,4 };
+
+	auto result = MaximumSumSubsequence<int>(array.data(), array.size());
+	for (auto i:result)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
 }
