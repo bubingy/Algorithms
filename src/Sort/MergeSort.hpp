a530dee9e115186890ce312c777be875ba043e46
bubingy <769004837@qq.com>
2021-07-21 14:54:38 +0800

    implement merge sort

--- src/Sort/MergeSort.hpp
+++ src/Sort/MergeSort.hpp
@@ -3,5 +3,66 @@
 #include "SortUtils.hpp"
 
 namespace mergesort {
+	template <typename t> void
+	Merge(t* array, size_t left, size_t mid, size_t right)
+	{
+		auto const subArrayOne = mid - left + 1;
+		auto const subArrayTwo = right - mid;
 
+		// Create temp arrays
+		auto* leftArray = new t[subArrayOne];
+		auto* rightArray = new t[subArrayTwo];
+
+		for (size_t i = 0; i < subArrayOne; i++)
+			leftArray[i] = array[left + i];
+		for (size_t j = 0; j < subArrayTwo; j++)
+			rightArray[j] = array[mid + 1 + j];
+
+		size_t indexOfSubArrayOne = 0; // Initial index of first sub-array
+		size_t indexOfSubArrayTwo = 0; // Initial index of second sub-array
+		size_t indexOfMergedArray = left; // Initial index of merged array
+
+		// Merge the temp arrays back into array[left..right]
+		while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
+			if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
+				array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+				indexOfSubArrayOne++;
+			}
+			else {
+				array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+				indexOfSubArrayTwo++;
+			}
+			indexOfMergedArray++;
+		}
+		// Copy the remaining elements of
+		// left[], if there are any
+		while (indexOfSubArrayOne < subArrayOne) {
+			array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+			indexOfSubArrayOne++;
+			indexOfMergedArray++;
+		}
+		// Copy the remaining elements of
+		// right[], if there are any
+		while (indexOfSubArrayTwo < subArrayTwo) {
+			array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+			indexOfSubArrayTwo++;
+			indexOfMergedArray++;
+		}
+	};
+
+	template <typename t> void
+	MergeSort(t* array, size_t begin, size_t end) {
+		if (begin >= end)
+			return;
+
+		size_t mid = begin + (end - begin) / 2;
+		MergeSort(array, begin, mid);
+		MergeSort(array, mid + 1, end);
+		Merge(array, begin, mid, end);
+	};
+
+	template <typename t> void
+	Sort(t* array, size_t array_size) {
+		MergeSort(array, 0, array_size - 1);
+	};
 }
--- src/example.cxx
+++ src/example.cxx
@@ -1,17 +1,13 @@
-#include <iostream>
-#include "MaximumSumSubsequence.hpp"
+#include <vector>
+#include "MergeSort.hpp"
 #include "print.hpp"
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	vector<int> array = { -2,1,-3,4,-1,2,1,-5,4 };
+	vector<int> array = { -2,1,-3,4,8,2,11,-5,14 };
 
-	auto result = MaximumSumSubsequence<int>(array.data(), array.size());
-	for (auto i:result)
-	{
-		cout << i << " ";
-	}
-	cout << endl;
+	mergesort::Sort(array.data(), array.size());
+	PrintArray(array.data(), array.size());
 }
