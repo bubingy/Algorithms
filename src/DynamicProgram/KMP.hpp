5a603b8b5169eca5236155ccf9c8ed29622ac9f1
bubingy <769004837@qq.com>
2021-11-29 17:55:30 +0800

    update kmp

--- src/DynamicProgram/KMP.hpp
+++ src/DynamicProgram/KMP.hpp
@@ -1,23 +1,24 @@
 #include <vector>
 
-size_t
+std::vector<size_t>
 MatchPattern(char* txt, size_t txt_length, char* parttern, size_t parttern_length)
 {
-	std::vector<std::vector<int>> dp
-	(parttern_length + 1, std::vector<int> (txt_length + 1, 0));
+	std::vector<std::vector<size_t>> dp
+	(parttern_length + 1, std::vector<size_t> (txt_length + 1, 0));
 	for (size_t j = 1; j <= parttern_length; j++)
 	{ 
 		for (size_t i = j; i <= txt_length; i++)
 		{
-			if (dp[j - 1][i - 1] != j - 1)
-				continue;
-			if (txt[i - 1] == parttern[j - 1])
-			{
-				dp[j][i] = dp[j - 1][i - 1] + 1;
-				if (dp[j][i] == parttern_length)
-					return i - parttern_length;
-			}
+			if (dp[j - 1][i - 1] != j - 1) continue;
+			if (txt[i - 1] == parttern[j - 1]) dp[j][i] = dp[j - 1][i - 1] + 1;
 		}
 	}
-	return -1;
+
+	std::vector<size_t> result;
+
+	for (size_t i = 1; i <= txt_length; i++)
+		if (dp[parttern_length][i] == parttern_length)
+			result.push_back(i - parttern_length);
+	
+	return result;
 }
--- src/example.cxx
+++ src/example.cxx
@@ -5,15 +5,20 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	char* txt = "ttafdijmemageaprgmaddf";
+	char* txt = "ttafdijmemageaprgmafdf";
 	size_t txt_length = 22;
-	char* parttern = "addf";
-	size_t pattern_length = 4;
-	auto index = MatchPattern(txt, txt_length, parttern, pattern_length);
-	for (size_t i = 0; i < txt_length; i++)
+	char* parttern = "afd";
+	size_t pattern_length = 3;
+	auto index_list = MatchPattern(txt, txt_length, parttern, pattern_length);
+	for (auto index : index_list)
 	{
-		if (i == index) cout << "{";
-		cout << txt[i];
-		if (i + 1 == index + pattern_length) cout << "}";
+		for (size_t i = 0; i < txt_length; i++)
+		{
+			if (i == index) cout << "{";
+			cout << txt[i];
+			if (i + 1 == index + pattern_length) cout << "}";
+		}
+		cout << endl;
 	}
+	
 }
