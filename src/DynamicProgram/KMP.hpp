8793ae8179032882a7881773393e522d47d2f1d3
bubingy <769004837@qq.com>
2021-12-03 13:28:32 +0800

    update kmp

--- src/DynamicProgram/KMP.hpp
+++ src/DynamicProgram/KMP.hpp
@@ -1,28 +1,38 @@
-#include <vector>
+#include <assert.h>
+#include <list>
 
-std::vector<size_t>
-MatchPattern(char* txt, size_t txt_length, char* parttern, size_t parttern_length)
+std::list<size_t>
+MatchPattern(char* txt, size_t txt_length, char* pattern, size_t pattern_length)
 {
-	std::vector<size_t> dp(txt_length + 1, 0);
-	size_t i = 0, j = 0, k = 0, temp = 0;
-	for (j = 0; j < parttern_length - 1; j++)
-	{ 
-		k = temp;
-		for (i = txt_length; i > k; i--)
-		{
-			if (dp[i - 1] == j && txt[i - 1] == parttern[j]) 
-			{
-				dp[i] = dp[i - 1] + 1;
-				temp = i;
-			}
-		}
-	}
+	assert(pattern_length > 0);
+	assert(txt_length > pattern_length);
+
+	std::list<size_t> dp;
+	std::list<size_t> temp_dp;
+	
+	for (size_t i = 0; i < txt_length; i++)
+		if (txt[i] == pattern[0])
+			dp.push_back(i);
+
+	if (pattern_length <= 1)
+		return dp;
 				
-	std::vector<size_t> result;
+	for (size_t i = 1; i < pattern_length; i++)
+	{
+		for (size_t j : dp)
+		{
+			if (j + 1 >= txt_length) break;
+			if (txt[j + 1] == pattern[i]) temp_dp.push_back(j + 1);
+		}
+		dp = temp_dp;
+		temp_dp.clear();
+	}
 
-	for (i = txt_length; i >= k - 1; i--)
-		if (dp[i - 1] == j && txt[i - 1] == parttern[j])
-			result.push_back(i - parttern_length);
-
-	return result;
+	std::list<size_t>::iterator iter = dp.begin();
+	while (iter != dp.end())
+	{
+		*iter = *iter + 1 - pattern_length;
+		iter++;
+	}
+	return dp;
 }
--- src/example.cxx
+++ src/example.cxx
@@ -1,22 +1,21 @@
 #include <iostream>
+#include <string>
 #include "KMP.hpp"
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	char* txt = "aaaaaaaaaafdee";
-	size_t txt_length = 14;
-	char* parttern = "aafde";
-	size_t pattern_length = 5;
-	auto index_list = MatchPattern(txt, txt_length, parttern, pattern_length);
+	string txt = "aaafdeaaaaafdee";
+	string pattern = "aafde";
+	auto index_list = MatchPattern(txt.data(), txt.size(), pattern.data(), pattern.size());
 	for (auto index : index_list)
 	{
-		for (size_t i = 0; i < txt_length; i++)
+		for (size_t i = 0; i < txt.size(); i++)
 		{
 			if (i == index) cout << "{";
 			cout << txt[i];
-			if (i + 1 == index + pattern_length) cout << "}";
+			if (i + 1 == index + pattern.size()) cout << "}";
 		}
 		cout << endl;
 	}
