d80d78ec232b5b89f1dbfd75f4b14c3d7698693a
bubingy <769004837@qq.com>
2021-12-28 10:02:14 +0800

    add Fibonacci

--- src/example.cxx
+++ src/example.cxx
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
-#include "IsPalindrome.hpp"
+#include "Fibonacci.h"
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	int tree_sequence[15] = {21, 14, 28, 11, 18, 25, 32, 5, 12, 15, 19, 23, 27, 30, 37};
-	bool result = IsPalindrome(123);
-	cout << result << endl;
+	Fibonacci* f = new Fibonacci();
+	for (size_t i = 1; i <= 10; i++)
+	{
+		cout << f->get(i) << endl;
+	}
+	
 }
